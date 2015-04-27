eaf06c07005e7d59edbb0da4c967d0d17b1d638f
Carsten Heinrigs <carsten@cahein.de>
2015-04-27 13:14:59 +0200

    fix override and style

--- app/overrides/layout.rb
+++ app/overrides/layout.rb
@@ -1,6 +1,6 @@
 # insert loading message
 Deface::Override.new(:virtual_path => 'spree/layouts/spree_application',
-                     :original => '9178f5dcd2363e3ff876de879c2eafe259f64038',
+                     :original => '28aff58816a0675ee013a39f632fa2b61404d0f9',
                      :name => 'append_loading_message',
-                     :insert_top => "#wrapper",
+                     :insert_top => '[data-hook="body"]',
                      :text => %q{<div id="progress"><%= image_tag 'admin/progress.gif' %> <%= Spree.t(:loading) %>...</div>})
