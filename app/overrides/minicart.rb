e40baf91b907b26110caab0c1404c34926e33ddb
Carsten Heinrigs <carsten@cahein.de>
2015-04-27 14:29:35 +0200

    fix deface original

--- app/overrides/minicart.rb
+++ app/overrides/minicart.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => 'spree/shared/_main_nav_bar',
-                     :original => '838e211947e51dd9e5056f685103c00b2094c5d2',
+                     :original => '4e64d76cfc69a498de6debd4410aa20bcafce5f6',
                      :name => 'add_mini_cart',
                      :insert_after => "#main-nav-bar",
                      :partial => "spree/shared/minicart")
