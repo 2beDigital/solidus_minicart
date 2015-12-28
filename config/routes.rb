5fff11e60bbd817718dad0a57652fba5da79cbc9
Noel <noel@2bedigital.com>
2015-12-28 11:31:55 +0000

    remove changes on routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Spree::Core::Engine.routes.draw do
-  get '/cart', :to => redirect('/404')
-  patch '/cart', :to => redirect('/404')
-  put '/cart/empty', :to => redirect('/404')
   # Add your extension routes here
 end
